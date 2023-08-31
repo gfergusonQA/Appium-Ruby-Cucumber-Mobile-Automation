e95122332b4589bcb5b55f7356bc10f093e058e6
GFQA <139098448+gfergusonQA@users.noreply.github.com>
2023-08-30 23:24:45 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Appium-with-Ruby-and-Cucumber
+ This project is to reproduce a real project from my last company using Ruby, Cucumber, and Appium to run tests on a mobile apps for both Android and iOS. 
---
+++ features/step_definitions/main_menu_steps.rb
@@ -0,0 +1,15 @@
+Given('I land on Home Screen') do
+    puts("Landed on Home Screen ")
+  end
+
+And('I press on menu icon') do
+    puts("Menu icon pressed")
+  end
+
+And('I should see left side menu') do
+    puts("I see left side menu")
+  end
+  
+
+
+  
